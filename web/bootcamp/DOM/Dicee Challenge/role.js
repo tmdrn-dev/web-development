4515b1d6dae08fbcd9f473f952726fa6a58f9cc2
tmdrnahs <37217634+tmdrnahs@users.noreply.github.com>
2021-09-19 19:41:31 +0900

    WebBootCamp: DOM, dicee challenge update!

--- web/bootcamp/DOM/Dicee Challenge/role.js
+++ web/bootcamp/DOM/Dicee Challenge/role.js
@@ -1,26 +1,31 @@
-let h1 = document.querySelector('h1');
-let diceImg = document.querySelectorAll(".row .col img");
+let h1 = document.querySelector("h1");
+let diceImg = document.querySelectorAll("img");
 
 function roleDice() {
-    return Math.floor(Math.random() * (6)) + 1;
+  return Math.floor(Math.random() * 6) + 1;
 }
 
 function getWinner(p1, p2) {
-    if (p1 > p2) {
-        return "Winner is player1";
-    } else if (p2 > p1) {
-        return "Winner is player2";
-    }
+  if (p1 > p2) {
+    return "Winner is player1";
+  } else if (p2 > p1) {
+    return "Winner is player2";
+  }
 
-    return "draw";
+  return "Draw";
 }
 
 function setDiceImg(idx, value) {
-    diceImg[idx].src = "images/dice" + value + ".png";
+  diceImg[idx].src = "images/dice" + value + ".png";
 }
 
-let player1 = roleDice();
-setDiceImg(0, player1)
-let player2 = roleDice();
-setDiceImg(1, player2)
-h1.innerText = getWinner(player1, player2);
+function onBtnClick() {
+  let player1 = roleDice();
+  setDiceImg(0, player1);
+  let player2 = roleDice();
+  setDiceImg(1, player2);
+  h1.innerText = getWinner(player1, player2);
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", onBtnClick);

2cb365a25dc0ea52295721df4a0d737acca6a37d
seungku-dev <seungku.dev@gmail.com>
2021-11-07 22:35:41 +0900

    WebBootCamp: express, responding to request with HTML

--- web/bootcamp/nodejs/calculator/calculator.js
+++ web/bootcamp/nodejs/calculator/calculator.js
@@ -1,11 +1,28 @@
 const express = require("express");
+
 const app = express();
-const port = 3000;
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(port, () => {
-  console.log(`calculator app listening at http://localhost:${port}`);
+app.post("/", (req, res) => {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+  let result = num1 + num2;
+  res.send(`Result is ${result}`);
 });
+
+app.get("/bmicalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", (req, res) => {
+  let weight = parseFloat(req.body.weight);
+  let height = parseFloat(req.body.height);
+  let bmi = weight / (height * weight);
+  res.send(`Result is ${bmi}`);
+});
+
+app.listen(3000);

a0a9d395d55a170efeef7d71af79f0b6b7bb4c1d
seungku-dev <seungku.dev@gmail.com>
2021-11-19 01:51:14 +0900

    WebBootCamp, todolist, add style

--- web/bootcamp/nodejs/todolist_v1/app.js
+++ web/bootcamp/nodejs/todolist_v1/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use("/", express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 

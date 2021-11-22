e594f72d31484c607e1bd6f9a9c2663df96d5c9e
seungku-dev <seungku.dev@gmail.com>
2021-11-23 01:58:10 +0900

    WebBootCamp, Blog challenge, Posting content to home route!

--- web/bootcamp/Challenges/Blog Website/app.js
+++ web/bootcamp/Challenges/Blog Website/app.js
@@ -15,11 +15,12 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+const posts = [];
+
 app.get("/", function (req, res) {
   res.render("home", {
     homeContent: homeContent,
-    aboutContent: aboutContent,
-    contactContent: contactContent,
+    postContent: posts,
   });
 });
 
@@ -41,11 +42,17 @@ app.get("/contact", function (req, res) {
 
 app.get("/compose", function (req, res) {
   res.render("compose");
-})
+});
 
 app.post("/compose", function (req, res) {
-  console.log(req.body);
-})
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+
+  posts.push(post);
+  res.redirect("/");
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");

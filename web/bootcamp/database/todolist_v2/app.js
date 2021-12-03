0e2e7ed1310c18e3facbcacfdb33f8698f6c5bf6
seungku-dev <seungku.dev@gmail.com>
2021-12-04 01:40:34 +0900

    WebBootCamp, #todolist_v2, Render items from DB, render new item

--- web/bootcamp/database/todolist_v2/app.js
+++ web/bootcamp/database/todolist_v2/app.js
@@ -10,55 +10,56 @@ mongoose.connect("mongodb://localhost:27017/todoListDB");
 const TodoItemSchem = { name: String };
 const TodoItem = mongoose.model("TodoItem", TodoItemSchem);
 
-const item_1 = new TodoItem({
-  name: "Sleep on time",
-});
-const item_2 = new TodoItem({
-  name: "Work hard",
-});
-const item_3 = new TodoItem({
-  name: "Study harder",
-});
+// const item_1 = new TodoItem({
+//   name: "Sleep on time",
+// });
+// const item_2 = new TodoItem({
+//   name: "Work hard",
+// });
+// const item_3 = new TodoItem({
+//   name: "Study harder",
+// });
 
-// const todoList = [item_1, item_2, item_3];
-// TodoItem.insertMany(todoList, function (err) {
+// const itemList = [item_1, item_2, item_3];
+// TodoItem.insertMany(itemList, function (err) {
 //   if (err) {
 //     console.error(err);
 //   }
 // });
 
-// Global variables
-const workList = [];
-
 app.get("/", function (req, res) {
-  res.render("list", { listTitle: "Today", todoList: todoList });
+  TodoItem.find(function (err, items) {
+    if (err) {
+      console.error(err);
+    } else {
+      res.render("list", { listTitle: "Today", todoList: items });
+    }
+  });
 });
 
 app.post("/", function (req, res) {
-  let item = req.body.newItem;
-  if (req.body.list === "work") {
-    workList.push(item);
-    res.redirect("/work");
-  } else {
-    todoList.push(item);
-    res.redirect("/");
-  }
+  let itemName = req.body.newItem;
+  const todoItem = new TodoItem({name: itemName});
+  todoItem.save();
+  res.redirect("/");
 });
 
-app.get("/work", function (req, res) {
-  res.render("list", { listTitle: "work List", todoList: workList });
-});
+// Global variables
+// const workList = [];
+// app.get("/work", function (req, res) {
+//   res.render("list", { listTitle: "work List", todoList: workList });
+// });
 
-app.post("/work", function (req, res) {
-  let item = req.body.newItem;
-  workList.push(item);
+// app.post("/work", function (req, res) {
+//   let item = req.body.newItem;
+//   workList.push(item);
 
-  res.redirect("/work");
-});
+//   res.redirect("/work");
+// });
 
-app.get("/about", function (req, res) {
-  res.render("about");
-});
+// app.get("/about", function (req, res) {
+//   res.render("about");
+// });
 
 app.listen(3000, function () {
   console.log("Server is running");

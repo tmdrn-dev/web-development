1e0d187aacf2dc736775cfdd02ac09b81cdec6ba
seungku-dev <seungku.dev@gmail.com>
2021-12-23 01:46:18 +0900

    WebBootCamp, challenge#3, Connect to MongoDB atlas

--- web/bootcamp/challenges/Adv Blog Website/app.js
+++ web/bootcamp/challenges/Adv Blog Website/app.js
@@ -1,31 +1,42 @@
 //jshint esversion:6
 
 const express = require("express");
-const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
+const account = require(__dirname + "/account.js");
+
 const app = express();
-
 app.set("view engine", "ejs");
-
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use("/", express.static(__dirname + "/public"));
 
-const systemDatabase = [
-  {
-    title: "home",
-    body: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.",
-  },
-  {
-    title: "about",
-    body: "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.",
-  },
-  {
-    title: "contact",
-    body: "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.",
-  },
-];
+mongoose.connect(
+  `mongodb+srv://${account.id}:${account.passwd}@cluster0.zzv9r.mongodb.net/blogDB?retryWrites=true&w=majority`
+);
 
-// Need to fix
+const contentSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+});
+
+const SystemContent = mongoose.model("systemcontent", contentSchema);
+const UserContent = mongoose.model("usercontent", contentSchema);
+
+const homeContent = new SystemContent({
+  title: "home",
+  body: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.",
+});
+const aboutContent = new SystemContent({
+  title: "home",
+  body: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.",
+});
+const contactContent = new SystemContent({
+  title: "contact",
+  body: "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.",
+});
+const systemContents = [homeContent, aboutContent, contactContent];
+
+// Need to Fix (findOne)
 function findContentByTitle(title, database) {
   return database.find((element) => element.title == title);
 }
@@ -35,6 +46,7 @@ function contentObject(content) {
   this.body = content.body;
 }
 
+// Need to Fix (insertMany or find)
 app.get("/", function (req, res) {
   const title = "home";
   const content = findContentByTitle(title, systemDatabase);

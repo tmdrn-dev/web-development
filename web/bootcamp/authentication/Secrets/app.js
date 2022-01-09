726c560494ced0def014f1a3963d8b891beb2f9f
seungku-dev <seungku.dev@gmail.com>
2022-01-09 22:52:10 +0900

    WebBootCamp, authentication, Access basic route

--- web/bootcamp/authentication/Secrets/app.js
+++ web/bootcamp/authentication/Secrets/app.js
@@ -1,1 +1,23 @@
-//jshint esversion:6
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.route("/").get(function (req, res) {
+  res.render("home");
+});
+
+app.route("/login").get(function (req, res) {
+  res.render("login");
+});
+
+app.route("/register").get(function (req, res) {
+  res.render("register");
+});
+
+const port = 3000;
+app.listen(port, function () {
+  console.log(`Server stated on port ${port}`);
+});

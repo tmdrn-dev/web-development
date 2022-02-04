140233a6d642b99ed72edf655e4bacd307c153e8
seungku.dev <seungku.dev@gmail.com>
2022-02-04 18:34:05 +0900

    WebBootCamp, React, expression in JSX

--- web/bootcamp/React/jsx/src/index.js
+++ web/bootcamp/React/jsx/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fName = "SK";
+const lName = "CHOI";
+
 ReactDOM.render(
   <div>
-    <h1>Hello world!</h1>
-    <p>This is parapraph</p>
+    <h1>Hello {fName} {lName}!</h1>
+    <p>My lucky number is {Math.floor(Math.random() * 10)}</p>
   </div>,
   document.getElementById("root")
 );

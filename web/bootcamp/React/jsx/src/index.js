d584dfb2e40f267edfe21b7157b563560c5b35a8
seungku.dev <seungku.dev@gmail.com>
2022-02-04 15:45:28 +0900

    WebBootCamp, React, my first app!
    npm init react-app jsx
    cd jsx
    npm start

---
+++ web/bootcamp/React/jsx/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello world!</h1>
+    <p>This is parapraph</p>
+  </div>,
+  document.getElementById("root")
+);

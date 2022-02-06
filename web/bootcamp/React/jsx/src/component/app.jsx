56991212f9497fdbe952ada3a9bf720503c72079
seungku-dev <seungku.dev@gmail.com>
2022-02-06 23:12:17 +0900

    WebBootCampe, React, React Components

---
+++ web/bootcamp/React/jsx/src/component/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./heading";
+import List from "./list";
+
+function App() {
+  return (
+    <div>
+      <Heading></Heading>
+      <List></List>
+    </div>
+  );
+}
+
+export default App;

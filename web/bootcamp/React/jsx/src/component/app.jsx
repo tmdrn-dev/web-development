cb58e35996fbe592cea83f2ab9ee357d44bace21
seungku-dev <seungku.dev@gmail.com>
2022-02-08 21:38:39 +0900

    WebBootCamp, React, React Components Practice

--- web/bootcamp/React/jsx/src/component/app.jsx
+++ web/bootcamp/React/jsx/src/component/app.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Heading from "./heading";
-import List from "./list";
 
 function App() {
   return (
     <div>
       <Heading></Heading>
-      <List></List>
     </div>
   );
 }

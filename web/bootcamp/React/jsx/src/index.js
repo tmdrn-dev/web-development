a677d7a79a76cccde8309e7eb84276a8e2fdf0c7
seungku-dev <seungku.dev@gmail.com>
2022-02-09 01:22:01 +0900

    WebBootCamp, React, ES6 Import/Export & Modules

--- web/bootcamp/React/jsx/src/index.js
+++ web/bootcamp/React/jsx/src/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./component/app";
+import { add, multiply, subtract, divide } from "./calculator";
 
-ReactDOM.render(<App></App>, document.getElementById("root"));
+//Import the add, multiply, subtract and divide functions
+//from the calculator.js file.
+//If successful, your website should look the same as the Final.png
+
+ReactDOM.render(
+  <ul>
+    <li>{add(1, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{divide(5, 2)}</li>
+  </ul>,
+  document.getElementById("root")
+);

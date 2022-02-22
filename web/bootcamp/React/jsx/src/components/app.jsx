fc143826cd4647a210071ebfd9010ab6ca19e5a3
seungku-dev <seungku.dev@gmail.com>
2022-02-23 01:57:42 +0900

    WebBootCamp, React, mapping data to components + React devtool
    Chrome react developer tool

--- web/bootcamp/React/jsx/src/components/app.jsx
+++ web/bootcamp/React/jsx/src/components/app.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Card from "./card";
-import contacts from "../contracts";
+import contacts from "../contacts";
+import Avatar from "./avatar";
+
+function createCard(contact) {
+  return <Card key={contact.id} info={contact} />;
+}
 
 function App() {
   return (
     <div>
+      <Avatar img="https://t1.daumcdn.net/cfile/tistory/99ECDF335E67C58135" />
       <h1 className="heading">My Contacts</h1>
-      {contacts.map((element) => (
+      {/* {contacts.map((element) => (
         <Card info={element} />
-      ))}
+      ))} */}
+      {contacts.map(createCard)}
     </div>
   );
 }

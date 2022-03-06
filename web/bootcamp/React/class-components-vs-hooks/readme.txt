a1d9682d430126fcd60ec2876054e3618c20a289
seungku-dev <seungku.dev@gmail.com>
2022-03-06 23:49:16 +0900

    WebBootCamp, React, Hook vs Class
    When you’re ready, we’d encourage you to start trying Hooks in new components you write. -from React-

---
+++ web/bootcamp/React/class-components-vs-hooks/readme.txt
@@ -0,0 +1,3 @@
+https://reactjs.org/docs/state-and-lifecycle.html
+https://reactjs.org/docs/hooks-intro.html
+https://reactjs.org/docs/hooks-faq.html#should-i-use-hooks-classes-or-a-mix-of-both
---
+++ web/bootcamp/React/class-components-vs-hooks/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ClassComponent from "./ClassComponent";
+import FunctionalComponent from "./FunctionalComponent";
+
+class App extends React.Component {
+  render() {
+    // return <ClassComponent />;
+    return <FunctionalComponent />;
+  }
+}
+
+export default App;
